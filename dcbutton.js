dc3598b2400749f5ceff004b57301bba654951c7
Mahdia Hashimy <mahdia@Mahdias-MacBook-Pro.local>
2017-08-14 10:46:47 -0400

    morevars

--- dcbutton.js
+++ dcbutton.js
@@ -1,11 +1,23 @@
+
+//var for people
 var selfRadio = $('#self');
 var coupleRadio = $('#couple');
 var familyRadio = $('#family');
 
+
+//var for days
 var oneday = $('#oneDay');
 var threedays= $('#threeDays');
 var fivedays= $('#fiveDays');
 
+//var for busy-ness of days
+var easyday = $('#easyDay');
+var mediumday= $('#mediumDay');
+var busyday= $('#busyDay');
+
+
+
+//var submitbutton
 
 var submitbutton =$('#submitButton');
 
@@ -33,7 +45,7 @@ function initMap() {
   });
 
 submitbutton.on('click',function(){
-  if(oneday.prop('checked') && selfRadio.prop('checked')) {
+  if(oneday.prop('checked') && selfRadio.prop('checked') && easyday.prop('checked')) {
   for (var i=0;i<DC_locations.length;i++){
       var location = DC_locations[i];
       var marker = new google.maps.Marker({
@@ -43,7 +55,7 @@ submitbutton.on('click',function(){
     }//for
   }
 
- else if(threedays.prop('checked') && coupleRadio.prop('checked')) {
+ else if(threedays.prop('checked') && coupleRadio.prop('checked') && mediumday.prop('checked')) {
   for (var i=0;i<DC_locations_busy_day.length;i++){
       var location = DC_locations_busy_day[i];
       var marker = new google.maps.Marker({
